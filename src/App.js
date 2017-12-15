5a335211f90759142f940e1740ba4c9c066d87bd
borodinchik <borodin.vladymyr@gmail.com>
2017-12-15 17:17:48 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { connect } from 'react-redux';
 
 class App extends Component {
-
   render() {
+    console.log(this.props.testStore);
+    var min = 0;
+    var max = 5;
+    var rand = Math.floor(Math.random() * max + min);
+    console.log('Random number', rand);
     return (
       <div className="container">
       <div className="row">
-        <div className="col-xs-6 col-md-4"><div className="circle one"></div></div>
-        <div className="col-xs-6 col-md-4"><div className="circle two"></div></div>
-        <div className="col-xs-6 col-md-4"><div className="circle three"></div></div>
+        <div className="col-xs-6 col-md-4" style={this.props.testStore}></div>
+        <div className="col-xs-6 col-md-4"></div>
+        <div className="col-xs-6 col-md-4" ></div>
         </div>
         <div className="row">
-          <div className="col-xs-6 col-md-4"><div className="circle four"></div></div>
-          <div className="col-xs-6 col-md-4"><div className="circle five"></div></div>
-          <div className="col-xs-6 col-md-4"><div className="circle six"></div></div>
+          <div className="col-xs-6 col-md-4"></div>
+          <div className="col-xs-6 col-md-4"></div>
+          <div className="col-xs-6 col-md-4"></div>
           </div>
       </div>
     );
   }
 }
 
-export default App;
+export default connect(
+  state => ({
+    testStore: state
+  }),
+  display => ({})
+)(App);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
-const bodySelector = document.querySelector('body');
-ReactDOM.render(<App />, bodySelector)
+var color =
+  {
+    backgroundColor: 'red',
+  }
+
+function game(state = color,action) {
+  if (action.type === 'ADD_ID') {
+    return [
+      ...state,
+      action.payload
+    ];
+    }
+    return state;
+}
+const store = createStore(game);
+
+const bodySelector = document.querySelector('#root');
+ReactDOM.render(
+  <Provider store={store}>
+  <App />
+  </Provider>,
+   bodySelector)
